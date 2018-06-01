e8db1051c5ec57d8cfc7d15c3bd4da9fb61cc8ac
Austyn Larkin <Quackventure@icloud.com>
2018-06-01 08:39:15 -0700

    Updates

---
+++ Assembly/commands.cpp
@@ -0,0 +1,7 @@
+int main() {
+    asm(
+        ""
+
+    );
+    return 0;
+}
--- Assembly/createTable.py
+++ Assembly/createTable.py
@@ -1,6 +1,92 @@
 # Make sure to install texttable
 from texttable import Texttable
 
+# How to format a number to output binary.
+# "{0:b}".format(123).zfill(8);
+
+allAssemblyInstructions = [
+{'name':'abs', 'type':'r'},
+
+{'name':'add', 'type':'r'},
+{'name':'addi', 'type':'i'},
+{'name':'addu', 'type':'r'},
+{'name':'addiu', 'type':'i'},
+
+{'name':'and', 'type':'r'},
+{'name':'andi', 'type':'i'},
+
+# These put results into the lo and hi registers
+#  and thus only use two arguments
+{'name':'div', 'type':'r', 'argcount':2},
+{'name':'divu', 'type':'r', 'argcount':2},
+
+{'name':'div', 'type':'r'},
+{'name':'divu', 'type':'r'},
+
+{'name':'mul', 'type':'r'},
+{'name':'mulo', 'type':'r'},
+{'name':'mulou', 'type':'r'},
+
+{'name':'mult', 'type':'r', 'argcount':2},
+{'name':'multu', 'type':'r', 'argcount':2},
+
+{'name':'neg', 'type':'r', 'argcount':2},
+{'name':'negu', 'type':'r', 'argcount':2},
+
+{'name':'nor', 'type':'r'},
+
+{'name':'not', 'type':'r', 'argcount':2},
+
+{'name':'or', 'type':'r'},
+{'name':'ori', 'type':'i'},
+
+{'name':'rem', 'type':'r'},
+{'name':'remu', 'type':'r'},
+
+{'name':'rol', 'type':'r'},
+{'name':'ror', 'type':'r'},
+
+{'name':'sll', 'type':'r'},
+{'name':'sllv', 'type':'r'},
+{'name':'sra', 'type':'r'},
+{'name':'srav', 'type':'r'},
+{'name':'srl', 'type':'r'},
+{'name':'srlv', 'type':'r'},
+
+{'name':'sub', 'type':'r'},
+{'name':'subu', 'type':'r'},
+
+{'name':'xor', 'type':'r'},
+{'name':'xori', 'type':'i'},
+
+
+{'name':'li', 'type':'i'},
+{'name':'lui', 'type':'i'},
+
+
+{'name':'seq', 'type':'r'},
+
+{'name':'sge', 'type':'r'},
+{'name':'sgeu', 'type':'r'},
+
+{'name':'sgt', 'type':'r'},
+{'name':'sgtu', 'type':'r'},
+
+{'name':'sle', 'type':'r'},
+{'name':'sleu', 'type':'r'},
+
+{'name':'slt', 'type':'r'},
+{'name':'slti', 'type':'i'},
+{'name':'sltu', 'type':'r'},
+{'name':'sltiu', 'type':'i'},
+
+{'name':'sne', 'type':'r'},
+
+
+{'name':'b', 'type':'j'},
+];
+
+
 table = Texttable()
 table.set_cols_align(["l", "r", "c"])
 table.set_cols_valign(["t", "m", "b"])
