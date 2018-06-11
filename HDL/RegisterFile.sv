49c153a87e8f291cde7faf9cb8c921f9d3401720
Austyn Larkin <Quackventure@icloud.com>
2018-06-11 09:31:51 -0400

    Updates

--- HDL/RegisterFile.sv
+++ HDL/RegisterFile.sv
@@ -18,4 +18,30 @@ output logic [31:0]readValue0,
 output logic [31:0]readValue1
 );
 
+// 31, 32 bit registers
+// The 32nd register is always zero.
+logic [31:0]registers[0:31];
+
+
+always_ff @(posedge clk or negedge rst) begin
+	if(rst == 1'b1) begin:reset
+		
+	end:reset
+	else
+	begin:clk
+
+	end:clk
+end
+
+// rs
+always_comb begin
+	unique case()
+
+	endcase
+end
+
+// rt
+
+// write
+
 endmodule
