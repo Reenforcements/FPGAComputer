6fcf299904dfe72427a6794801d4d02016cad7e7
Austyn Larkin <Quackventure@icloud.com>
2018-07-03 11:56:14 -0400

    Updates

--- ModelSimTestData/processorTest1.asm
+++ ModelSimTestData/processorTest1.asm
@@ -1,9 +1,9 @@
 
 # _0_ nop (ALU funct = 0x0)
-# _0_ mfhi (ALU funct = 0x10)
-# _0_ mthi (ALU funct = 0x11)
-# _0_ mflo (ALU funct = 0x12)
-# _0_ mtlo (ALU funct = 0x13)
+# _1_ mfhi (ALU funct = 0x10)
+# _1_ mthi (ALU funct = 0x11)
+# _1_ mflo (ALU funct = 0x12)
+# _1_ mtlo (ALU funct = 0x13)
 # _1_ mult (ALU funct = 0x18)
 # _0_ multu (ALU funct = 0x19)
 # _1_ div (ALU funct = 0x1a)
@@ -12,13 +12,13 @@
 # _1_ addu (ALU funct = 0x21)
 # _1_ sub (ALU funct = 0x22)
 # _1_ subu (ALU funct = 0x23)
-# _0_ and (ALU funct = 0x24)
+# _1_ and (ALU funct = 0x24)
 # _0_ move (ALU funct = 0x25)
-# _0_ or (ALU funct = 0x25)
-# _0_ xor (ALU funct = 0x26)
-# _0_ nor (ALU funct = 0x27)
-# _0_ slt (ALU funct = 0x2a)
-# _0_ sltu (ALU funct = 0x2b)
+# _1_ or (ALU funct = 0x25)
+# _1_ xor (ALU funct = 0x26)
+# _1_ nor (ALU funct = 0x27)
+# _1_ slt (ALU funct = 0x2a)
+# _1_ sltu (ALU funct = 0x2b)
 # _0_ sllv (ALU funct = 0x4)
 # _0_ srlv (ALU funct = 0x6)
 # _0_ srav (ALU funct = 0x7)
@@ -55,13 +55,13 @@
 # _0_ bgtz (ALU funct = 0x39)
 # _1_ addi (ALU funct = 0x24)
 # _1_ addiu (ALU funct = 0x38)
-# _0_ li (ALU funct = 0x3f)
-# _0_ slti (ALU funct = 0x24)
-# _0_ sltiu (ALU funct = 0x24)
-# _0_ andi (ALU funct = 0x24)
-# _0_ ori (ALU funct = 0x24)
-# _0_ xori (ALU funct = 0x24)
-# _0_ lui (ALU funct = 0x0)
+# _1_ li (ALU funct = 0x3f)
+# _1_ slti (ALU funct = 0x24)
+# _1_ sltiu (ALU funct = 0x24)
+# _1_ andi (ALU funct = 0x24)
+# _1_ ori (ALU funct = 0x24)
+# _1_ xori (ALU funct = 0x24)
+# _1_ lui (ALU funct = 0x0)
 
 .data
   namePrompt: .asciiz "Please enter your name: "
@@ -183,6 +183,88 @@
 	sw $s5, 0($a0)
 		addi $a0, $a0, 4
 
+	# Bitwise (and, or, xor, andi, ori, xori)
+
+	li $t0, 0xCCCC
+	li $t1, 0x3333
+	li $t2, 0xFFFF
+
+	and $s0, $t0, $t1
+	and $s1, $t1, $t2
+	or  $s2, $t0, $t1
+	xor $s3, $t2, $t1
+	andi $s4, $t0, 0xFFFF
+	ori $s5, $t1, 0x4444
+	xori $s6, $t2, 0x1111
+	nor $s7, $t0, $t1
+	
+
+	sw $s0, 0($a0)
+		addi $a0, $a0, 4
+	sw $s1, 0($a0)
+		addi $a0, $a0, 4
+	sw $s2, 0($a0)
+		addi $a0, $a0, 4
+	sw $s3, 0($a0)
+		addi $a0, $a0, 4
+	sw $s4, 0($a0)
+		addi $a0, $a0, 4
+	sw $s5, 0($a0)
+		addi $a0, $a0, 4
+	sw $s6, 0($a0)
+		addi $a0, $a0, 4
+	sw $s7, 0($a0)
+		addi $a0, $a0, 4
+
+	# LO and HI
+
+	lui $t0, 0xABCD
+	ori $t0, $t0, 0xEF12
+	
+	lui $t1, 0xFFFF
+	ori $t1, $t1, 0xFFFF
+
+	mthi $t1
+	mtlo $t0
+
+	mfhi $s1
+	mflo $s0
+
+	sw $s0, 0($a0)
+		addi $a0, $a0, 4
+	sw $s1, 0($a0)
+		addi $a0, $a0, 4
+
+
+	# SLT
+	
+	li $t0, -1
+	li $t1, 1
+	li $t2, 2
+	
+	slt $s0, $t0, $t1
+	slt $s1, $0, $t0
+	slt $s2, $0, $t1
+	sltu $s3, $t1, $t0
+	slti $s4, $t2, 3 
+	slti $s5, $t2, -1 
+	sltiu $s6, $t2, -1
+
+	sw $s0, 0($a0)
+		addi $a0, $a0, 4
+	sw $s1, 0($a0)
+		addi $a0, $a0, 4
+	sw $s2, 0($a0)
+		addi $a0, $a0, 4
+	sw $s3, 0($a0)
+		addi $a0, $a0, 4
+	sw $s4, 0($a0)
+		addi $a0, $a0, 4
+	sw $s5, 0($a0)
+		addi $a0, $a0, 4
+	sw $s6, 0($a0)
+		addi $a0, $a0, 4
+	
 
 	# Loop forever so things don't break.
 	section1:
