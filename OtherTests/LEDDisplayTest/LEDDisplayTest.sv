5fcacc74e216f1915836e73379681cf0c4c515db
Austyn Larkin <Quackventure@icloud.com>
2018-08-06 16:56:32 -0400

    LEDDriver on FPGA working!

--- OtherTests/LEDDisplayTest/LEDDisplayTest.sv
+++ OtherTests/LEDDisplayTest/LEDDisplayTest.sv
@@ -1,22 +1,53 @@
 module LEDDisplayTest(
-	input logic clk,
+	input logic clkIn,
 	input logic rst,
 	
-	output logic [3:0]rowDecoder,
+	output logic [4:0]rowDecoder,
 	output logic pixelClk,
-	output logic [2:0]columnPixelsUpper,
-	output logic [2:0]columnPixelsLower,
+	output logic [2:0]columnPixels0,
+	output logic [2:0]columnPixels1,
 	output logic columnLatch,
 	output logic blank
 );
 
-logic [10:0]pixelAddress;
-logic [7:0]pixel;
+logic clk;
+logic [7:0]clkCounter;
+always_ff @ (posedge clkIn or negedge rst) begin
+	if (rst == 1'b0) begin
+		clk <= 1'b1;
+		clkCounter <= 8'd1;
+	end
+	else begin
+		if (clkCounter == 8'd3) begin
+			clk <= ~clk;
+			clkCounter <= 8'd1;
+		end
+		else begin
+			clkCounter <= clkCounter + 8'd1;
+		end
+	end
+end
+
+
+	
+logic [10:0]pixelAddress0;
+logic [7:0]pixel0;
+	
+logic [10:0]pixelAddress1;
+logic [7:0]pixel1;
+	
+// This will be used to swap buffers outside of this module.
+logic done;
 	
 
 LEDDisplay ld(
 	.*
 );
 
+always_comb begin
+	// Easiest test data on Earth.
+	pixel0 = 8'b00110000;//pixelAddress0[7:0];
+	pixel1 = 8'b00110000;//pixelAddress1[7:0];
+end
 
 endmodule
---
+++ OtherTests/LEDDisplayTest/output_files/LEDDisplayTest.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 18.0.0 Build 614 04/24/2018 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("/home/imaustyn/Desktop/FPGAComputer/OtherTests/LEDDisplayTest/output_files/") File("LEDDisplayTest.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
